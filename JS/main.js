69152de7b7019ff83a9c0a98976c8595926c41e0
Harshal Gami <77873685+harshalgami13@users.noreply.github.com>
2021-10-28 15:52:59 +0530

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,74 +1,90 @@
 $(document).ready(function () {
     $('.more1').click(function () {
-        $('.more-link1').slideToggle(500)
-        if ($('.more').text()=='See') {
-            $('.more').text('See')
+        $('.more-link1').slideToggle(200)
+        if ($('.more1').text() == 'See More ...') {
+            $('.more1').text('... See Less')
         }
-        else{
-            $('.more').text('See')
+        else {
+            $('.more1').text('See More ...')
         }
     });
     $('.more2').click(function () {
-        $('.more-link2').slideToggle(500)
-        if ($('.more').text()=='See') {
-            $('.more').text('See')
+        $('.more-link2').slideToggle(200)
+        if ($('.more2').text() == 'See More ...') {
+            $('.more2').text('... See Less')
         }
-        else{
-            $('.more').text('See')
+        else {
+            $('.more2').text('See More ...')
         }
     });
     $('.more3').click(function () {
-        $('.more-link3').slideToggle(500)
-        if ($('.more').text()=='See') {
-            $('.more').text('See')
+        $('.more-link3').slideToggle(200)
+        if ($('.more3').text() == 'See More ...') {
+            $('.more3').text('... See Less')
         }
-        else{
-            $('.more').text('See')
+        else {
+            $('.more3').text('See More ...')
         }
     });
     $('.more4').click(function () {
-        $('.more-link4').slideToggle(500)
-        if ($('.more').text()=='See') {
-            $('.more').text('See')
+        $('.more-link4').slideToggle(200)
+        if ($('.more4').text() == 'See More ...') {
+            $('.more4').text('... See Less')
         }
-        else{
-            $('.more').text('See')
+        else {
+            $('.more4').text('See More ...')
         }
     });
     $('.more5').click(function () {
-        $('.more-link5').slideToggle(500)
-        if ($('.more').text()=='See') {
-            $('.more').text('See')
+        $('.more-link5').slideToggle(200)
+        if ($('.more5').text() == 'See More ...') {
+            $('.more5').text('... See Less')
         }
-        else{
-            $('.more').text('See')
+        else {
+            $('.more5').text('See More ...')
         }
     });
     $('.more6').click(function () {
-        $('.more-link6').slideToggle(500)
-        if ($('.more').text()=='See') {
-            $('.more').text('See')
+        $('.more-link6').slideToggle(200)
+        if ($('.more6').text() == 'See More ...') {
+            $('.more6').text('... See Less')
         }
-        else{
-            $('.more').text('See')
+        else {
+            $('.more6').text('See More ...')
         }
     });
     $('.more7').click(function () {
-        $('.more-link7').slideToggle(500)
-        if ($('.more').text()=='See') {
-            $('.more').text('See')
+        $('.more-link7').slideToggle(200)
+        if ($('.more7').text() == 'See More ...') {
+            $('.more7').text('... See Less')
         }
-        else{
-            $('.more').text('See')
+        else {
+            $('.more7').text('See More ...')
         }
     });
 
-    $(window).scroll(function(){
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
+    // scroll-up button show/hide script
+    $(window).scroll(function () {
+        if (this.scrollY > 150) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
+        if (this.scrollY > 150) {
+            $('.social').addClass("show");
+        } else {
+            $('.social').removeClass("show");
+        }
+        if (this.scrollY > 0) {
+            $('.header').addClass("fixed");
+        } else {
+            $('.header').removeClass("fixed");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        $('html').css("scrollBehavior", "auto");
     });
 });
